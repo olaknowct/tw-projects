0e8ce4352484da84dada2e3687b046875a453eb7
Christopher Olano <olanochristopher@gmail.com>
2023-04-14 17:09:43 +0800

    shortly: shortening the link functionality

--- shortly/js/script.js
+++ shortly/js/script.js
@@ -1,5 +1,11 @@
 const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
+const errMsg = document.getElementById('err-msg');
+const shortenButton = document.querySelector('.form-shorten__btn');
+const formShortenInput = document.querySelector('.form-shorten__input');
+const formShorten = document.querySelector('.form-shorten');
+const loadSpinner = document.querySelector('.load-spinner');
+
 let vwidth;
 
 function navToggle() {
@@ -22,3 +28,62 @@ const removeBurgerNav = () => {
 // window.onload = calcBrowserWith;
 window.onresize = calcBrowserWith;
 btn.addEventListener('click', navToggle);
+
+const isValidHttpUrl = (string) => {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (e) {
+    return false;
+  }
+
+  return url.protocol === 'http:' || url.protocol === 'https:';
+};
+
+shortenButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const link = formShortenInput.value;
+  errMsg.innerHTML = '';
+
+  if (link === '') {
+    errMsg.innerHTML = 'Please enter something';
+    formShortenInput.classList.add('border-red');
+    return;
+  }
+
+  if (!isValidHttpUrl(link)) {
+    errMsg.innerHTML = 'Not a valid Link';
+    formShortenInput.classList.add('border-red');
+    return;
+  }
+
+  const url = `https://api.shrtco.de/v2/shorten?url=${link}/very/long/link.html`;
+
+  async function getShortLink() {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data.result;
+  }
+
+  loadSpinner.classList.remove('hidden');
+
+  getShortLink().then((res) => createShortenLinkEl(res));
+});
+
+function createShortenLinkEl(data) {
+  // factsList.insertAdjacentHTML("afterbegin", "<li>chr</li>");
+  const { full_short_link, original_link } = data;
+  const html = `      
+  <li
+    class="flex group flex-col items-center rounded-lg bg-white px-6 py-6 font-bold md:flex-row md:justify-between md:gap-4"
+  >
+    <a href="">${original_link} </a>
+    <a href="" class="text-cyan md:ml-auto">${full_short_link} </a>
+    <button class="group-odd:bg-cyan group-even:bg-darkViolet mt-2 rounded-lg bg-cyan px-6 py-2 text-white hover:opacity-50 md:mt-0">
+      Copy
+    </button>
+  </li>`;
+
+  formShorten.insertAdjacentHTML('afterend', html);
+}

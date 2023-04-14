d119ec14ec91844efbec796827455af762d2825d
Christopher Olano <olanochristopher@gmail.com>
2023-04-14 17:35:15 +0800

    shortly: refactory, create dom js for dom elements selector

---
+++ shortly/js/dom.js
@@ -0,0 +1,10 @@
+export const DOM = {
+  // DOM Elements
+  btn: document.getElementById('menu-btn'),
+  menu: document.getElementById('menu'),
+  errMsg: document.getElementById('err-msg'),
+  shortenButton: document.querySelector('.form-shorten__btn'),
+  formShortenInput: document.querySelector('.form-shorten__input'),
+  formShorten: document.querySelector('.form-shorten'),
+  loadSpinner: document.querySelector('.load-spinner'),
+};
